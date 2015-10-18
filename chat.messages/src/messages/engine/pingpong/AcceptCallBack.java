54c9db784c6e51d56caefe0945e92f7bc1ed7a78
quentin <Quentin.Torck@e.ujf-grenoble.fr>
2015-10-18 12:32:41 +0200

    d

--- chat.messages/src/messages/engine/pingpong/AcceptCallBack.java
+++ chat.messages/src/messages/engine/pingpong/AcceptCallBack.java
@@ -8,12 +8,15 @@ public class AcceptCallBack implements AcceptCallback {
 
 	@Override
 	public void accepted(Server server, Channel channel) {
+		
+		
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void closed(Channel channel) {
+		channel.close();
 		// TODO Auto-generated method stub
 		
 	}
--- chat.messages/src/messages/engine/pingpong/ConnectCallBack.java
+++ chat.messages/src/messages/engine/pingpong/ConnectCallBack.java
@@ -2,17 +2,20 @@ package messages.engine.pingpong;
 
 import messages.engine.Channel;
 import messages.engine.ConnectCallback;
+import messages.engine.DeliverCallback;
 
 public class ConnectCallBack implements ConnectCallback{
-
+	DeliverCallback bc ;
 	@Override
 	public void closed(Channel channel) {
+		channel.close();
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void connected(Channel channel) {
+		channel.setDeliverCallback(bc);
 		// TODO Auto-generated method stub
 		
 	}
